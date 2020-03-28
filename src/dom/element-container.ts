25873f7dba5dece7d759a06f1be8d32a51de9055
liuxinyi12 <liuxinyi12>
2020-03-28 16:35:46 +0800

    fix: 设置了transform:none之后，恢复原来的样式

--- src/dom/element-container.ts
+++ src/dom/element-container.ts
@@ -2,7 +2,7 @@ import {CSSParsedDeclaration} from '../css/index';
 import {TextContainer} from './text-container';
 import {Bounds, parseBounds} from '../css/layout/bounds';
 import {isHTMLElementNode} from './node-parser';
-
+type cacheTransformType = string | null;
 export const enum FLAGS {
     CREATES_STACKING_CONTEXT = 1 << 1,
     CREATES_REAL_STACKING_CONTEXT = 1 << 2,
@@ -20,11 +20,17 @@ export class ElementContainer {
         this.styles = new CSSParsedDeclaration(window.getComputedStyle(element, null));
         this.textNodes = [];
         this.elements = [];
+        let cacheTransform: cacheTransformType = null;
         if (this.styles.transform !== null && isHTMLElementNode(element)) {
             // getBoundingClientRect takes transforms into account
-            // element.style.transform = 'none';
+            cacheTransform = element.style.transform;
+            element.style.transform = 'none';
         }
         this.bounds = parseBounds(element);
+        if (cacheTransform !== null && isHTMLElementNode(element)) {
+            // getBoundingClientRect takes transforms into account
+            element.style.transform = cacheTransform;
+        }
         this.flags = 0;
     }
 }
